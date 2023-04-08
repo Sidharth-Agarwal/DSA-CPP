2fd7e51902bb7cf23e1aee0d423105d74253a399
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-04-08 12:36:58 +0530

    Length

--- 1. Linked Lists/10LengthOfLinkedList.cpp
+++ 1. Linked Lists/10LengthOfLinkedList.cpp
@@ -16,3 +16,54 @@ class Node
     }
 };
 
+Node *takeInput()
+{
+    int data;
+    cin >> data;
+
+    Node *head = NULL;
+    Node *tail = NULL;
+    
+    while(data!=-1)
+    {
+        Node *newNode = new Node(data);
+
+        if(head == NULL)
+        {
+            head = newNode;
+            tail = newNode;
+        }
+
+        else
+        {
+            tail->next = newNode;
+            tail = newNode;
+        }
+
+        cin >> data;
+    }
+
+    return head;
+}
+
+int length(Node *head)
+{
+    int count = 0;
+
+    while(head!=NULL)
+    {
+        head = head->next;
+        count++;
+    }
+
+    return count;
+}
+
+int main()
+{
+    Node *head = takeInput();
+
+    int len = length(head);
+
+    cout << "The length of the linked list is : " << len;
+}

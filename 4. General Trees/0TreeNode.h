0d87b0abfe042305fbb0946aaad103ada33e172b
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-04-26 06:04:21 +0530

    Update

---
+++ 4. General Trees/0TreeNode.h
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+template<typename T>
+class Node
+{
+    public:
+    T data;
+    vector<Node<T> *> children;
+
+    Node(T data)
+    {
+        this->data = data;
+    }
+
+    ~Node()
+    {
+        for(int i=0;i<children.size())
+        {
+            delete chilren[i];
+        }
+    }
+};

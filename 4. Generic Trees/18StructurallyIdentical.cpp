16ccc6e1c46563ac8c937c1757e411d7fd25e8be
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-05-04 18:01:37 +0530

    Structurally Identical

--- 4. Generic Trees/18StructurallyIdentical.cpp
+++ 4. Generic Trees/18StructurallyIdentical.cpp
@@ -69,8 +69,53 @@ void printTree(TreeNode<int>* root)
     }
 }
 
+bool areIdentical(TreeNode<int> *root1, TreeNode<int> *root2)
+{
+    if(root1 == NULL && root2 == NULL)
+    {
+        return true;
+    }
+
+    if((root1 == NULL && root2 != NULL) || (root1 != NULL && root2 == NULL))
+    {
+        return false;
+    }
+
+    if((root1->data != root2->data) || (root1->children.size() != root2->children.size()))
+    {
+        return false;
+    }
+
+    for (int i = 0; i < root1->children.size(); i++)
+    {
+        TreeNode<int> *child1 = root1->children[i];
+        TreeNode<int> *child2 = root2->children[i];
+
+        if(!areIdentical(child1,child2))
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 int main()
 {
-    TreeNode<int> *root = takeInput();
-    printTree(root);
+    cout << "Enter the data of the first tree";
+    cout << endl;
+    TreeNode<int> *root1 = takeInput();
+
+    cout << "Enter the data of the second tree";
+    cout << endl;
+    TreeNode<int>* root2 = takeInput();
+
+    if(areIdentical(root1,root2))
+    {
+        cout << "The trees are identical";
+    }
+    else
+    {
+        cout << "The tree are not identical";
+    }
 }

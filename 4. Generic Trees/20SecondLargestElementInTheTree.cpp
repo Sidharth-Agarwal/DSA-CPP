0d82b825fef66414a851c3f3eb34ea295a543ad8
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-05-04 22:06:51 +0530

    SecondLargestNode

--- 4. Generic Trees/20SecondLargestElementInTheTree.cpp
+++ 4. Generic Trees/20SecondLargestElementInTheTree.cpp
@@ -68,7 +68,73 @@ void printTree(TreeNode<int>* root)
     }
 }
 
+template<typename T>
+class SecondLargestReturnType
+{
+    public:
+    TreeNode<int> *largest;
+    TreeNode<int> *secondLargest;
 
+    SecondLargestReturnType(TreeNode<int>* largest, TreeNode<int>* secondLargest)
+    {
+        this->largest = largest;
+        this->secondLargest = secondLargest;
+    }
+};
+
+SecondLargestReturnType<int>* getSecondLargestNodeHeleper(TreeNode<int>* root)
+{
+    if(root == NULL)
+    {
+        return new SecondLargestReturnType<int>(NULL, NULL);
+    }
+
+    SecondLargestReturnType<int> *ans = new SecondLargestReturnType<int>(root, NULL);
+
+    for(int i=0;i<root->children.size();i++)
+    {
+        SecondLargestReturnType<int> *childAns = getSecondLargestNodeHeleper(root->children[i]);
+
+        if(childAns->largest->data > ans->largest->data)
+        {
+            if(childAns->secondLargest == NULL)
+            {
+                ans->secondLargest = ans->largest;
+                ans->largest = childAns->largest;
+            }
+
+            else
+            {
+                if(childAns->secondLargest->data > ans->largest->data)
+                {
+                    ans->secondLargest = childAns->secondLargest;
+                    ans->largest = childAns->largest;
+                }
+
+                else
+                {
+                    ans->secondLargest = ans->largest;
+                    ans->largest = childAns->largest;
+                }
+            }
+        }
+
+        else
+        {
+            if(ans->largest->data!=childAns->largest->data && 
+            (ans->secondLargest == NULL || childAns->largest->data > ans->secondLargest->data))
+            {
+                ans->secondLargest = childAns->largest;
+            }
+        }
+    }
+    return ans;
+}
+
+TreeNode<int>* getSecondLargestNode(TreeNode<int>* root)
+{
+    return getSecondLargestNodeHeleper(root)->secondLargest;
+}
 
 int main()
 {

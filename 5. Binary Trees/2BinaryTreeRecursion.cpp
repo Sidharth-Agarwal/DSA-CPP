9a394698796d6092d8a4aa94e42b7ff840360e85
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-05-06 18:08:00 +0530

    Binary Tree Recursive

--- 5. Binary Trees/2BinaryTreeRecursion.cpp
+++ 5. Binary Trees/2BinaryTreeRecursion.cpp
@@ -24,8 +24,33 @@ BinaryTreeNode<int>* takeInput()
     return root;
 }
 
+void printTree(BinaryTreeNode<int>* root)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+
+    cout << root->data << " : ";
+
+    if(root->left != NULL)
+    {
+        cout << "Left : " << root->left->data << " ";
+    }
+
+    if(root->right != NULL)
+    {
+        cout << "Right : " << root->right->data << " ";
+    }
+
+    cout << endl;
+
+    printTree(root->left);
+    printTree(root->right);
+}
+
 int main()
 {
     BinaryTreeNode<int> *root = takeInput();
-    //printTree(root);
+    printTree(root);
 }

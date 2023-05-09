831a9aee4958e56220020ab9e77848a2a61bdea8
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-05-10 00:14:54 +0530

    Diameter of Binary Tree

---
+++ 5. Binary Trees/17DiameterOfBinaryTree.cpp
@@ -0,0 +1,134 @@
+#include<iostream>
+#include<bits/stdc++.h>
+#include"0BinaryTreeNode.h"
+
+using namespace std;
+BinaryTreeNode<int>* takeInput()
+{
+    int rootData;
+    cout << "Enter the root data : ";
+    cin >> rootData;
+
+    BinaryTreeNode<int> *root = new BinaryTreeNode<int>(rootData);
+
+    queue<BinaryTreeNode<int> *> pendingNodes;
+    pendingNodes.push(root);
+
+    while(pendingNodes.size()!=0)
+    {
+        BinaryTreeNode<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+
+        cout << "Enter the left child of " << front->data << " : ";
+        int leftChildData;
+        cin >> leftChildData;
+
+        if(leftChildData!=-1)
+        {
+            BinaryTreeNode<int> *leftChild = new BinaryTreeNode<int>(leftChildData);
+            pendingNodes.push(leftChild);
+            front->left = leftChild;
+        }
+
+        cout << "Enter the right child of " << front->data << " : ";
+        int rightChildData;
+        cin >> rightChildData;
+
+        if(rightChildData!=-1)
+        {
+            BinaryTreeNode<int> *rightChild = new BinaryTreeNode<int>(rightChildData);
+            pendingNodes.push(rightChild);
+            front->right = rightChild;
+        }
+    }
+
+    return root;
+}
+
+void print(BinaryTreeNode<int>* root)
+{
+    if(root == NULL)
+    {
+        return;
+    }
+
+    queue<BinaryTreeNode<int> *> pendingNodes;
+    pendingNodes.push(root);
+
+    while(pendingNodes.size()!=0)
+    {
+        BinaryTreeNode<int> *front = pendingNodes.front();
+        pendingNodes.pop();
+
+        if(front == NULL)
+        {
+            cout<<endl;
+
+            if(!pendingNodes.empty())
+            {
+                pendingNodes.push(NULL);
+            }
+        }
+
+        else
+        {
+            cout << front->data << ":";
+            cout << "L:";
+
+            if(front->left)
+            {
+                pendingNodes.push(front->left);
+                cout << front->left->data << ",";
+            }
+            else
+            {
+                cout << -1 << ",";
+            }
+
+            cout << "R:";
+
+            if(front->right)
+            {
+                pendingNodes.push(front->right);
+                cout << front->right->data << endl;
+            }
+            else
+            {
+                cout << -1 << endl;
+            }
+        }
+    }
+}
+
+int height(BinaryTreeNode<int>* root)
+{
+    if(!root)
+    {
+        return 0;
+    }
+
+    return 1 + max(height(root->left), height(root->right));
+}
+
+int diameter(BinaryTreeNode<int>* root)
+{
+    if(root == NULL)
+    {
+        return 0;
+    }
+
+    int op1 = diameter(root->left);
+    int op2 = diameter(root->right);
+    int op3 = height(root->left) + height(root->right) + 1;
+
+    int ans = max(op3, max(op1, op2));
+    return ans;
+}
+
+int main()
+{
+    BinaryTreeNode<int> *root = takeInput();
+    print(root);
+
+    cout << "The diameter of the binary tree is : " << diameter(root);
+}

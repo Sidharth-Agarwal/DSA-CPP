5be9fdf2897287eaef728051f3a336b409ddb9f6
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-05-16 23:18:35 +0530

    Queue using arrays

---
+++ 3. Queues/1QueueImplementationByStaticArray.cpp
@@ -0,0 +1,106 @@
+#include<iostream>
+#include<climits>
+
+using namespace std;
+
+class Queue
+{
+    int *data;
+    int nextIndex;
+    int firstIndex;
+    int size;
+    int capacity;
+
+    public:
+
+    Queue(int s)
+    {
+        data = new int[s];
+        nextIndex = 0;
+        firstIndex = -1;
+        size = 0;
+        capacity = s;
+    }
+
+    int getSize()
+    {
+        return size;
+    }
+
+    bool isEmpty()
+    {
+        return size == 0;
+    }
+
+    int front()
+    {
+        if(size == 0)
+        {
+            cout << "Queue is empty";
+            return INT_MIN;
+        }
+
+        return data[firstIndex];
+    }
+
+    void enqueue(int element)
+    {
+        if(size == capacity)
+        {
+            cout << "Queue is full" << endl;
+            return;
+        }
+
+        data[nextIndex] = element;
+        nextIndex = (nextIndex + 1) % capacity;
+
+        if(firstIndex == -1)
+        {
+            firstIndex = 0;
+        }
+
+        size++;
+    }
+
+    int dequeue()
+    {
+        if(isEmpty())
+        {
+            cout << "Queue is empty";
+            return INT_MIN;
+        }
+
+        int ans = data[firstIndex];
+        firstIndex = (firstIndex + 1) % capacity;
+        size--;
+
+        if(size == 0)
+        {
+            firstIndex = -1;
+            nextIndex = 0;
+        }
+
+        return ans;
+    }
+};
+
+int main()
+{
+    Queue q(5);
+
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    q.enqueue(40);
+    q.enqueue(50);
+    q.enqueue(60);
+
+    cout<<q.front()<<endl;
+    cout<<q.dequeue()<<endl;
+    cout<<q.dequeue()<<endl;
+    cout<<q.dequeue()<<endl;
+
+    cout<<q.getSize()<<endl;
+
+    cout<<q.isEmpty()<<endl;
+}

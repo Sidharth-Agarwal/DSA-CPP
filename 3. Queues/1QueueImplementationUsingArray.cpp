6638f41667b2c1cbf177558544f92881d59c9292
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-05-23 23:46:55 +0530

    Simple Queue

---
+++ 3. Queues/1QueueImplementationUsingArray.cpp
@@ -0,0 +1,105 @@
+#include<iostream>
+#include<climits>
+
+using namespace std;
+
+class Queue
+{
+    int *arr;
+    int front;
+    int rear;
+    int size;
+    int capacity;
+
+    public:
+    Queue(int s)
+    {
+        arr = new int[s];
+        size = 0;
+        capacity = s;
+        front = 0;
+        rear = 0;
+    }
+
+    bool isEmpty()
+    {
+        return front == rear;
+    }
+
+    int getSize()
+    {
+        return size;
+    }
+
+    void enqueue(int element)
+    {
+        if(rear == capacity)
+        {
+            cout << "Queue is full!";
+        }
+
+        else
+        {
+            arr[rear] = element;
+            rear++;
+            size++;
+        }
+    }
+
+    int dequeue()
+    {
+        if(front == rear)
+        {
+            return INT_MIN;
+        }
+
+        else
+        {
+            int ans = arr[front];
+            front++;
+            size--;
+
+            if (front == rear)
+            {
+                front = 0;
+                rear = 0;
+            }
+
+            return ans;
+        }
+    }
+
+    int front()
+    {
+        if(isEmpty())
+        {
+            cout << "Queue is empty!";
+        }
+
+        else
+        {
+            return arr[front];
+        }
+    }
+};
+
+int main()
+{
+    Queue q(5);
+
+    q.enqueue(10);
+    q.enqueue(20);
+    q.enqueue(30);
+    q.enqueue(40);
+    q.enqueue(50);
+    q.enqueue(60);
+
+    cout<<q.front()<<endl;
+    cout<<q.dequeue()<<endl;
+    cout<<q.dequeue()<<endl;
+    cout<<q.dequeue()<<endl;
+
+    cout<<q.getSize()<<endl;
+
+    cout<<q.isEmpty()<<endl;
+}

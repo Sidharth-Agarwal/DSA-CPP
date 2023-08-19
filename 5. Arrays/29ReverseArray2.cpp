48fcf3f7d87d049475d894f74e76a7b9ea5ddeee
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-08-19 11:17:26 +0530

    Reverse Array

--- 5. Arrays/29ReverseArray2.cpp
+++ 5. Arrays/29ReverseArray2.cpp
@@ -6,16 +6,16 @@ using namespace std;
 
 void reverse(int *arr, int start, int end)
 {
-    if(start<end)
+    if(start>=end)
     {
-        int temp = arr[start];
-        arr[start] = arr[end];
-        arr[end] = temp;
-        start++;
-        end--;
-
-        reverse(arr,start,end);
+        return;
     }
+
+    int temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+
+    reverse(arr,start+1,end-1);
 }
 
 void reverseArray(int *arr, int n)

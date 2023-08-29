65b4c390660f7e0e57748f5ffe5184bfb24f4308
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-08-29 11:58:03 +0530

    XOR

---
+++ 3. Bit Manipulation/3XOROfNumbersTillN-1.cpp
@@ -0,0 +1,21 @@
+// Bit Manipulation
+
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"Enter the number : ";
+    cin>>n;
+
+    int ans = 0;
+
+    for(int i=0;i<=n;i++)
+    {
+        ans = ans^i;
+    }
+
+    cout<<"The output is : "<<ans;
+}

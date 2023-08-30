10b14a39222766cef2c5fb914457b8840a4c550f
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-08-30 10:08:50 +0530

    Extract bit

---
+++ 3. Bit Manipulation/8ExtractithBit.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+
+    cout<<"Enter the number : ";
+    cin>>n;
+
+    int i;
+    
+    cout<<"Enter the value of i : ";
+    cin>>i;
+
+    
+}

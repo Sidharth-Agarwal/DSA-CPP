958914c3f40f82ec1cf7839d8f1a3c7cd2915497
Sidharth-Agarwal <sidharthagarwal.2003@gmail.com>
2023-08-31 12:16:49 +0530

    Insert LL

--- 10. Linked Lists/2InsertNodeWithoutReturn.cpp
+++ 10. Linked Lists/2InsertNodeWithoutReturn.cpp
@@ -59,15 +59,58 @@ void print(Node* head)
     }
 }
 
-void insert(Node* &head)
-{
-    
+void insert(Node* &head,int i, int data)
+{   
+    // If linked list is empty
+    if(head == NULL)
+    {
+        Node* newNode = new Node(data);
+        head = newNode;
+    }
+
+    Node* newNode = new Node(data);
+
+    // To insert a node at the start
+    if(i==0)
+    {
+        newNode->next = head;
+        head = newNode;
+    }
+
+    int count = 0;
+    Node* temp = head;
+
+    // To stop a node before
+    while(count < i-1 && temp!=NULL)
+    {
+        temp = temp->next;
+        count++;
+    }
+
+    // Inserting the node at the position
+    if(temp!=0)
+    {
+        Node *sub = temp->next;
+        temp->next = newNode;
+        newNode->next = sub;
+    }
 }
 
 int main()
 {
+    cout<<"Enter the linked list : ";
     Node *head = takeInput();
 
-    cout << "The entered linked list is : ";
+    int data;
+    cout<<"Enter the node to be inserted : ";
+    cin>>data;
+
+    int i;
+    cout<<"Enter the position of the data : ";
+    cin>>i;
+
+    insert(head,i,data);
+
+    cout<<"Updated linked list is : ";
     print(head);
 }
